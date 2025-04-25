53bd27e3feb66258bb06ade76dd80de586fb99a0
yujiro mori <yuyu14191567@gmail.com>
2025-04-25 22:56:22 +0900

    Instance Addelement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ButtonEvent from './Button';
 import Mause from './DetectMause';
+import InstanceTag from './InstanceTag';
 
 function App() {
   return (
     <div className="App">
       {/* <button onClick={ButtonEvent}> ボタン</button> */}
-      <Mause></Mause>
+      <InstanceTag></InstanceTag>
     </div>
   );
 }
