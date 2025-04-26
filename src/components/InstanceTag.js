d05cdd0a09966e3a15e008c1c0759e8e582f3851
yujiro mori <yuyu14191567@gmail.com>
2025-04-26 20:16:12 +0900

    make save and textarea

--- src/components/InstanceTag.js
+++ src/components/InstanceTag.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Mause from "./DetectMause";
+
 const InstanceTag = () =>{
     const [elements, setElements] = useState([]);
     const [text, setText] = useState(""); 
